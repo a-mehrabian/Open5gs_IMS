5e07350d92b226279bf1edb2e9ff6b4c9d913c73
RanoND <rabih@humanitas.io>
2024-01-11 10:18:23 -0500

    add build script

---
+++ build_images.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "Choose an option:"
+echo "1: Build basic (Core network)"
+echo "2: Build eNB"
+echo "3: Build all (CN + eNB)"
+echo "4: Update with cache for all images"
+read option
+
+case $option in
+  1)
+    # Build basic (Core network)
+    cd base
+    docker build --no-cache --force-rm -t docker_open5gs .
+    
+    cd ../ims_base
+    docker build --no-cache --force-rm -t docker_kamailio .
+    ;;
+    
+  2)
+    # Build eNB
+    cd ../srslte
+    docker build --no-cache --force-rm -t docker_srslte .
+    ;;
+
+  3)
+    # Build all (CN + eNB)
+    cd base
+    docker build --no-cache --force-rm -t docker_open5gs .
+    
+    cd ../ims_base
+    docker build --no-cache --force-rm -t docker_kamailio .
+
+    cd ../srslte
+    docker build --no-cache --force-rm -t docker_srslte .
+    ;;
+
+  4)
+    # Update with cache for all images
+    cd base
+    docker build -t docker_open5gs .
+    
+    cd ../ims_base
+    docker build -t docker_kamailio .
+
+    cd ../srslte
+    docker build -t docker_srslte .
+    ;;
+
+  *)
+    echo "Invalid option"
+    ;;
+esac
+
+# Clear junk images
+docker image prune --force
+docker compose build
